355c9dc559a6e4e1c9e71c2a017400df24471a06
hhd389 <huahoangduy.389@gmail.com>
2021-05-12 10:17:19 +0700

    change sidebar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 const openSidebar = document.querySelector('.right-menu');
 const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
 openSidebar.addEventListener('click',(e) => {
-    console.log("yeah, u click me...");
     openSidebar.classList.toggle("active");
     sidebar.classList.toggle('open-sidebar');
+    overlay.classList.toggle('active');
+
+});
+if(overlay){
+    overlay.addEventListener('click',(e) => {
+        openSidebar.classList.remove("active");
+        sidebar.classList.remove('open-sidebar');
+        overlay.classList.remove('active');
+    });
+}
+// dropdown item
+const dropDownMenu = document.querySelectorAll('.has-child');
+dropDownMenu.forEach(menu => {
+    menu.addEventListener('click',function(e){
+        menu.classList.toggle("open");
+        const icon = menu.children[0].children[0];
+        const menuItem = menu.children[1];
+        const height = menuItem.scrollHeight;
+        console.log("Height : " + height);
+        if(menu.classList.contains("open")){
+            console.log("open");
+            menuItem.style.maxHeight = height + "px";
+            icon.style.transform = "rotate(90deg)";
+        }
+        else{
+            console.log("no open");
+            menuItem.removeAttribute("style");
+            icon.style.transform = "rotate(0deg)";
+        }
+        
+    });
 });
